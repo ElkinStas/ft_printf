c437c67e686f0ec96dcb425177295f12406f1abd
Bradly Hudson <bhudson@mi-w1.21-school.ru>
2019-04-10 18:15:32 +0300

    updt

---
+++ mein/ft_indent4.c
@@ -0,0 +1,70 @@
+
+# include "printf.h"
+
+void    fucnctcheck(char **p, t_flist *base)
+{
+    functflag2(p, base);
+    if ((base->spec == 1 || base->spec == 5|| base->spec == 2) && (base->match = 1))
+         (*p)++;
+    if ((base->spec == 3 || base->spec == 4) && (base->match = 1))
+        (*p) = (*p) + 2;
+}
+void    ft_checkstr(char *sval, int *check)
+{
+    *check = 0;
+    while (*sval)
+    {
+        (*check)++;
+        sval++;
+    }
+}
+void    ft_printtext(char *sval, t_flist *base, int check)
+{
+    ft_checkstr(sval, &check);
+          if(base->point >0 && check != 0)
+       {
+           base->indent = base->indent - base->point;
+            if (check != 0)
+            check = base->point;
+       }
+        if (base->indent > 0)
+        ft_indentd2(&base, check);
+    while(check > 0)
+    { 
+        ft_putchar(*sval);
+        sval++;
+        check--;
+        (base->count)++;
+    }
+    if (base->sign == '-')
+        ft_minus2(&base);   
+}
+void    functionstring(va_list ap, t_flist *base)
+{
+    char    *sval;
+    int     check;
+    check = 0;
+
+     sval = va_arg(ap, char *);
+     if (sval == NULL)
+       { 
+           ft_putstr("(null)");
+            (base->count)= (base->count) + 6;
+       }
+     else 
+         ft_printtext(sval, base, check);
+}
+
+void    ft_indent4(t_flist **base)
+{
+    if((*base)->indent2 != '0')
+            {
+                ft_putchar(' '); 
+                ((*base)->count)++;
+            }
+            else if ((*base)->resh == 1)
+            {
+                ft_putchar('0'); 
+                ((*base)->count)++;
+            }
+}
---
+++ mein/functionindent5.c
@@ -0,0 +1,84 @@
+
+# include "printf.h"
+
+void    ft_uputnbr(void* z, t_flist **base)
+{
+    unsigned long long y;
+    unsigned long long a;
+     if (((*base)->spec == 1) && (y = (unsigned short) z))
+       y = (unsigned short)y;
+    else if (((*base)->spec == 2) && (y = (unsigned long) z))
+       y = (unsigned long) y;
+    else if ((*base)->spec == 4 && (y = (unsigned long long) z))
+        y = (unsigned long long) y;
+    else if ((*base)->spec == 0 && (y = (unsigned int) z))
+     y = (unsigned int) y;
+    else if ((*base)->spec == 3 && (y = (char) z))
+        y = (char) y;
+    a = y;
+		if (a >= 10)
+		    ft_uputnbr((void*)(a / 10), base); 
+	ft_putchar((char)(a % 10 + '0'));
+    ((*base)->count)++;
+}
+ void    ft_checklongitudeu(void*  z, t_flist **base, unsigned long long *checkmin)
+{
+    if (((*base)->spec == 1) && (*checkmin = (unsigned short) z))
+          *checkmin  = (unsigned short)*checkmin ;
+    else if (((*base)->spec  == 2) && (*checkmin = (unsigned long) z))
+       *checkmin  = (unsigned long) *checkmin ;
+    else if ((*base)->spec == 4 && (*checkmin  = (unsigned long long) z))
+        *checkmin  = (unsigned long long) *checkmin ;
+    else if ((*base)->spec  == 0 && (*checkmin  = (unsigned int) z))
+           *checkmin  = (unsigned int) *checkmin ;
+     else if ((*base)->spec == 3 && (*checkmin  = (unsigned char) z))
+        *checkmin  = (unsigned char) *checkmin ;
+    while((*checkmin) != 0) 
+    {
+        (*checkmin) = (*checkmin)/10;
+        (*base)->longitude++;
+    }
+     (*base)->point =(*base)->point - (*base)->longitude;
+}
+void    ft_checkminu(void* z, t_flist **base, unsigned long long *checkmin)
+{
+    if (((*base)->spec == 1) && (*checkmin = (unsigned short) z))
+          *checkmin  = (unsigned short)*checkmin ;
+    else if (((*base)->spec  == 2) && (*checkmin = (unsigned long) z))
+       *checkmin  = (unsigned long) *checkmin ;
+    else if ((*base)->spec == 4 && (*checkmin  = (unsigned long long) z))
+        *checkmin  = (unsigned long long) *checkmin ;
+    else if ((*base)->spec  == 0 && (*checkmin  = (unsigned int) z))
+           *checkmin  = (unsigned int) *checkmin ;
+     else if ((*base)->spec == 3 && (*checkmin  = (unsigned char) z))
+        *checkmin  = (unsigned char) *checkmin ;
+    
+}
+void    ft_loputnbrindentprintu(t_flist *base, int check)
+{
+    int i;
+    i = base->indent;
+    if (check < i)
+    {
+        i = i - check;
+        
+        while(i > 0 && base->sign != '-')
+        {
+            if(base->zero == '0')
+                ft_putchar('0');
+            else
+                ft_putchar(' ');
+            i --;
+            base->count++;
+        }
+    }
+}
+void    functionindent5(t_flist *base, int *check)
+{
+    if(base->zero == '0')
+    {
+        ft_putchar('+');
+        (base)->count++;
+    }
+    check++;
+}
